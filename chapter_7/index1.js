27da1ea929fb404f9da00f71aae62f743381ee5b
chi335 <chi5268@naver.com>
2025-04-10 13:49:19 +0900

    Add files via upload

---
+++ chapter_7/index1.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Counter2 from './chapter_07/Counter2';
+const root = document.getElementById("root");
+ReactDOM.createRoot(root).render(<Counter2 />);
